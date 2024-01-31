9e0761f72d9efd422ce57883840cfc91eddd8a47
thefrostypixel <87690743+thefrostypixel@users.noreply.github.com>
2024-01-31 21:44:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var controllerWindow;
+var main = window;
 var controller;
 function openController() {
-    controllerWindow = window.open("controller.html", "Controller", "width=1920,height=1080,left=" + (window.outerWidth - 1920) / 2 + ",top=" + (window.outerHeight - 1080) / 2);
+    document.getElementById("controller-button").innerText = "STEUERUNG SCHLIEßEN 􀺾";
+    document.getElementById("controller-button").setAttribute("onclick", "main.closeController();");
     window.addEventListener("unload", closeController);
-    document.getElementById("open-controller-button").innerText = "STEUERUNG SCHLIEßEN 􀺾";
-    document.getElementById("open-controller-button").onclick = closeController;
+    controller = window.open("controller.html", "Controller", "width=1920,height=1080,left=" + (window.outerWidth - 1920) / 2 + ",top=" + (window.outerHeight - 1080) / 2);
 }
 function closeController() {
-    controllerWindow.close();
-    controllerWindow = undefined;
+    controller.close();
     controller = undefined;
-    document.getElementById("open-controller-button").innerText = "STEUERUNG ÖFFNEN 􀑍";
-    document.getElementById("open-controller-button").onclick = openController;
+    document.getElementById("controller-button").innerText = "STEUERUNG ÖFFNEN 􀑍";
+    document.getElementById("controller-button").setAttribute("onclick", "main.openController();");
 }
-document.addEventListener("keydown", onKeyPress);
-document.addEventListener("click", onMouseClick);
+document.body.addEventListener("keydown", onKeyPress);
+document.body.addEventListener("click", onMouseClick);
 function onKeyPress(event) {
     if (event.key == " " || event.key == "Enter" || event.key == "Tab" || event.key == "D" || event.key == "d" || event.key == "W" || event.key == "w" || event.key == "ArrowRight" || event.key == "ArrowUp") {
         nextSlide();
@@ -30,17 +29,7 @@ function onKeyPress(event) {
         }
         event.preventDefault();
     } else if (event.key == "f") {
-        if (event.target.baseURI.includes("controller")) {
-            if (controller.fullscreenElement) {
-                controller.exitFullscreen();
-                controller.getElementById("fullscreen-button").innerText = "VOLLBLID AKTIVIEREN 􀅊";
-            } else {
-                controller.documentElement.requestFullscreen();
-                controller.getElementById("fullscreen-button").innerText = "VOLLBLID DEAKTIVIEREN 􀅋";
-            }
-        } else {
-            toggleFullscreen();
-        }
+        toggleFullscreen(event.target.baseURI.includes("controller") ? controller.document : document);
         event.preventDefault();
     }
 }
@@ -49,69 +38,17 @@ function onMouseClick(event) {
         nextSlide();
     }
 }
-function toggleFullscreen() {
-    if (document.fullscreenElement) {
-        document.exitFullscreen();
-        document.getElementById("fullscreen-button").innerText = "VOLLBLID AKTIVIEREN 􀅊";
+function toggleFullscreen(doc) {
+    if (doc.fullscreenElement) {
+        doc.exitFullscreen();
+        doc.getElementById("fullscreen-button").innerText = "VOLLBLID AKTIVIEREN 􀅊";
     } else {
-        document.documentElement.requestFullscreen();
-        document.getElementById("fullscreen-button").innerText = "VOLLBLID DEAKTIVIEREN 􀅋";
+        doc.documentElement.requestFullscreen();
+        doc.getElementById("fullscreen-button").innerText = "VOLLBLID DEAKTIVIEREN 􀅋";
     }
 }
 
 var slide = 0;
-/* function displaySlide() {
-    if (document.querySelector(".slide-" + slide) == undefined) {
-        slide--;
-        return;
-    }
-    var slideClass = ".slide-" + slide;
-    document.body.setAttribute("slide", slide);
-    document.body.setAttribute("style", "--slide: " + slide);
-    if (controller != undefined) {
-        controller.body.setAttribute("slide", slide);
-        controller.body.setAttribute("style", "--slide: " + slide);
-    }
-     // Not Using :has(), Works For Older Browsers
-    for (var i = 0; i < slide; i++) {
-        querySelect(".slide-" + i).forEach((element) => {
-            element.setAttribute("gone", "");
-        });
-    }
-    // Using :has(), Does Not Work For Older Browsers
-    //querySelect(".slide:has(.slide-" + (slide - 1) + "), .slide-" + (slide - 1)).forEach((element) => {
-    //    element.setAttribute("gone", "");
-    //});
-    querySelect(".slide-" + slide).forEach((element) => {
-        element.removeAttribute("invisible");
-        element.removeAttribute("gone");
-        // Not Using :has(), Works For Older Browsers
-        var parent = element.parentNode;
-        while (parent != undefined && parent.tagName != "BODY") {
-            parent.removeAttribute("gone");
-            parent = parent.parentNode;
-        }
-        // Using :has(), Does Not Work For Older Browsers
-        //querySelect(".slide:has(.slide-" + slide + "), .slide-" + slide).forEach((element) => {
-        //    element.removeAttribute("gone");
-        //});
-        if (element.tagName == "svg") {
-            animate(element);
-        }
-        element.querySelectorAll("svg:not(.slide):not(" + slideClass + " .slide *)").forEach((svg) => {
-            animate(svg);
-        });
-    });
-    querySelect(".slide-" + (slide + 1)).forEach((element) => {
-        element.setAttribute("invisible", "");
-        if (element.tagName == "svg") {
-            resetAnimation(element);
-        }
-        element.querySelectorAll("svg:not(.slide):not(.slide-" + slide + " .slide *)").forEach((svg) => {
-            resetAnimation(svg);
-        });
-    });
-} */
 function nextSlide() {
     slide++;
     if (document.querySelector(".slide-" + slide) == undefined) {
@@ -177,17 +114,23 @@ function querySelect(selector) {
     if (controller == undefined) {
         return Array.from(document.querySelectorAll(selector));
     } else {
-        return Array.from(document.querySelectorAll(selector)).concat(Array.from(controller.querySelectorAll(selector)));
+        return Array.from(document.querySelectorAll(selector)).concat(Array.from(controller.document.querySelectorAll(selector)));
     }
 }
 function animate(svg) {
-    svg.querySelectorAll("animate, animateColor, animateMotion, animateTransform").forEach((anim) => setTimeout(() => {
-        anim.beginElement();
-    }, Number(anim.getAttribute("start")) * 1000));
+    svg.querySelectorAll("animate, animateColor, animateMotion, animateTransform").forEach((anim) => {
+        if (anim.hasAttribute("start")) {
+            setTimeout(() => {
+                anim.beginElement();
+            }, Number(anim.getAttribute("start")) * 1000);
+        }
+    });
 }
 function resetAnimation(svg) {
     svg.querySelectorAll("animate, animateColor, animateMotion, animateTransform").forEach((anim) => {
-        anim.parentNode.replaceChild(anim.cloneNode(), anim);
+        if (anim.hasAttribute("start")) {
+            anim.parentNode.replaceChild(anim.cloneNode(), anim);
+        }
     });
 }
 
@@ -198,3 +141,7 @@ var startSlide = 0;
 for (var i = 0; i < startSlide; i++) {
     nextSlide();
 }
+
+/* TODO
+- Add Time And Other Important Info (Like Slide Number) To Controller
+*/
